c17593aba1ec5112e8efd857149b7645df9b39b6
avinaash <avinaash.mani@gmail.com>
2024-07-10 17:38:37 +0530

    test_1 on the real life robot

--- noetic_ws/src/esp32_button/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/avinaash/noetic_ws/src/esp32_button
-# Build directory: /home/avinaash/noetic_ws/src/esp32_button/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
