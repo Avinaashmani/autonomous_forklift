e98533cc13a5aa61a02d0696d490f09559052cf9
avinaash <avinaash.mani@gmail.com>
2024-07-24 10:40:37 +0530

    started work on tricycle controller for ros1 noetic

---
+++ noetic_ws/build/gearfork_bot/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/avinaash/autonomous_forklift/noetic_ws/src/gearfork_bot
+# Build directory: /home/avinaash/autonomous_forklift/noetic_ws/build/gearfork_bot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
